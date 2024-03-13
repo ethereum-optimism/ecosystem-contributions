19649b720cc847ddcb3795760bf9f4a8b3c75a40
jarkis0m <163348097+jarkis0m@users.noreply.github.com>
2024-03-13 21:07:22 +0100

    Update processMarkDown.mjs

--- data/processMarkDown.mjs
+++ data/processMarkDown.mjs
@@ -3,27 +3,33 @@ import path from 'path';
 import matter from 'gray-matter';
 
 
+// Asynchronously fetches and processes markdown files in a specified directory,
+// then writes the processed data to a JSON file.
 export async function getResources() {
+    // Define the directory path where markdown files are located.
     const directoryPath = path.join(process.cwd(), 'public', 'static', 'markdown');
+    
+    // Read the directory to get the list of file names.
     const fileNames = await fs.readdir(directoryPath);
 
-    let postDataPromises = [];
+    // Initialize an array to hold promises for processing markdown files.
+    const postDataPromises = fileNames.filter(fileName => path.extname(fileName).toLowerCase() === ".md")
+                                      .map(fileName => getMarkDownData(fileName));
 
-    for (let i = 0; i < fileNames.length; i++) {
-      if(path.extname(fileNames[i]).toLowerCase() === ".md") {
-        postDataPromises.push(getMarkDownData(fileNames[i]))
-      }
-    }
-      
+    // Wait for all markdown files to be processed.
     const postData = await Promise.all(postDataPromises);
 
+    // Convert the processed data to a JSON string with indentation for readability.
     const jsonData = JSON.stringify(postData, null, 2);
 
+    // Define the output path for the JSON file.
     const outputPath = path.join(process.cwd(), './public/static/json/output.json');
     
+    // Write the JSON data to the output file.
     await fs.writeFile(outputPath, jsonData);
 
-    // return outputPath;
+    // Return the path of the output file.
+    return outputPath;
 }
 
 // Promise<MarkDownData>
