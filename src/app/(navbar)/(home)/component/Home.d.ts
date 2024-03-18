993992bd78e8185b0f4c42c7bdb06e6c0c930a4c
tgrziminiar <mixzazazaza1212@gmail.com>
2024-03-18 14:44:00 +0700

    update metadata title

--- src/app/(navbar)/(home)/component/Home.d.ts
+++ src/app/(navbar)/(home)/component/Home.d.ts
@@ -33,8 +33,8 @@ export interface MarkDownData extends LoadMarkDownType {
     category: string;
     effort: string;
     labels: string[];
-    skillsets: string[];
-    contribution: {
+    "skill-sets": string[];
+    contributions: {
         contributors: string[],
         "discussion-link": string,
         links: string[];
