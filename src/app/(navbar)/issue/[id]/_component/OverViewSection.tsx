75c723c6b31d4511b10c6207ce8f40ff9c35540b
Billy19191 <bill.ratchanon@gmail.com>
2024-03-27 22:23:55 +0700

    feat: github idea link

--- src/app/(navbar)/issue/[id]/_component/OverViewSection.tsx
+++ src/app/(navbar)/issue/[id]/_component/OverViewSection.tsx
@@ -7,8 +7,9 @@ import { categoryKeyAndValue } from '../../../(home)/Text'
 import NavigateIcon from '@/public/icon/NavigateIcon'
 import Link from 'next/link'
 import Home from '@/public/icon/HomeIcon'
+import GitHubIcon from '@/public/icon/github-mark'
 
-export function handleExecuteStatus(status: string) {
+function handleExecuteStatus(status: string) {
   switch (status) {
     case 'not-started':
       return (
@@ -69,12 +70,15 @@ export default function OverViewSection({
       id="Overview"
       className="border-0 bg-white p-5 sm:p-6 md:p-10 lg:rounded-md lg:border items-center w-auto  lg:mb-6"
     >
-      {/* <Link href="/#project" className="flex gap-2 items-center ">
+      <Link
+        href="/#project"
+        className="flex gap-2 items-center ml-1 mb-6 lg:hidden"
+      >
         <Home width="16" height="16" />
         <h6 className="text-sm font-medium text-gray-700 hover:underline">
           Home
         </h6>
-      </Link> */}
+      </Link>
 
       <div
         data-tooltip-id={content.contributions['execution-status']}
@@ -96,7 +100,9 @@ export default function OverViewSection({
         />
       </div>
 
-      <h6 className="text-3xl font-bold my-4 ">{content.title}</h6>
+      <h6 className="flex flex-row flex-wrap text-3xl font-bold my-4 ">
+        <div className="flex">{content.title}</div>
+      </h6>
       <hr className="hidden lg:block my-6  border-t border-gray-300" />
       <p className=" text-gray-500 text-base font-normal">
         {content.description}
@@ -205,9 +211,9 @@ export default function OverViewSection({
             ))
           )}
         </div>
-        <div className="flex flex-row gap-3">
+        <div className="flex flex-row flex-wrap items-center gap-3 mt-4">
           <Link
-            className="w-fit mt-4 border px-8 py-3 bg-[#ff0420] text-white rounded-3xl hover:bg-white hover:text-[#ff0420] hover:border-[#ff0420] transition ease-linear duration-300 shadow-md"
+            className="w-fit border px-8 py-3 bg-[#ff0420] text-white rounded-3xl hover:bg-white hover:text-[#ff0420] hover:border-[#ff0420] transition ease-linear duration-300 shadow-md"
             href={
               content.contributions['discussion-link'] != ''
                 ? content.contributions['discussion-link']
@@ -231,7 +237,7 @@ export default function OverViewSection({
           </Link>
 
           <Link
-            className="w-fit mt-4 border px-8 py-3 bg-black text-white rounded-3xl hover:invert transition ease-linear duration-300 shadow-md"
+            className="w-fit border px-8 py-3 bg-black text-white rounded-3xl hover:invert transition ease-linear duration-300 shadow-md"
             // href={`https://github.com/ethereum-optimism/ecosystem-contributions/blob/main/contributions/${content.id}.md`}
             href={{
               pathname: '/docs/claim-an-idea.md',
@@ -243,7 +249,7 @@ export default function OverViewSection({
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="1.3em"
-                height="1.5em"
+                height="1.55em"
                 viewBox="0 0 24 24"
               >
                 <path
@@ -253,6 +259,24 @@ export default function OverViewSection({
               </svg>
             </button>
           </Link>
+          <div className="flex-grow"></div>
+          <div>
+            <Link
+              href={`https://github.com/ethereum-optimism/ecosystem-contributions/blob/main/contributions/${content.id}.md`}
+              target="_blank"
+              className="flex flex-row justify-center items-center mr-4 text-slate-700 hover:underline transition ease-linear duration-300"
+            >
+              {/* <div className="mr-1.5">View</div> */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+              >
+                <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"></path>
+              </svg>
+            </Link>
+          </div>
           {/* <Link
             className="w-fit mt-4 px-8 py-3 text-black rounded-3xl transition ease-linear duration-300 "
             href={{
