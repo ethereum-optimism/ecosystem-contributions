0f342a7f3afd3c3ea5197b94e4ee9680931ebb98
wit03 <witjarukit@gmail.com>
2024-08-03 02:03:47 +0700

    feat: page_view tracking poc

--- src/app/component/routeListener.tsx
+++ src/app/component/routeListener.tsx
@@ -2,12 +2,15 @@
 
 import { usePathname } from 'next/navigation';
 import { useEffect } from 'react';
+import sendToMixpanel from '../lib/sendToMixpanel';
 
 export function RouteChangeListener() {
   const pathname = usePathname();
 
   useEffect(() => {
       // for MixPanel implementation
+      console.log("route change to: ", pathname);
+      sendToMixpanel("page_view")
 
   }, [pathname])
 
