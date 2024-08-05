211c7e2ac79cb9414c0c87a860f4c1df462af65b
Billy19191 <bill.ratchanon@gmail.com>
2024-08-06 01:19:23 +0700

    Revert "fix: no such file or directory for "/json'"

--- src/app/component/routeListener.tsx
+++ src/app/component/routeListener.tsx
@@ -1,22 +1,24 @@
-'use client'
+'use client';
 
-import { usePathname } from 'next/navigation'
-import { useEffect, useRef } from 'react'
-import sendToMixpanel from '../lib/sendToMixpanel'
-import {
-  PAGE_VIEW,
-  PROJECTNAME_PROPERTY,
-} from '@/public/static/mixpanel/event-name'
+import { usePathname } from 'next/navigation';
+import { useEffect, useRef } from 'react';
+import sendToMixpanel from '../lib/sendToMixpanel';
+import { PAGE_VIEW, PROJECTNAME_PROPERTY } from '@/public/static/mixpanel/event-name';
 
 export function RouteChangeListener() {
-  let pathname = usePathname()
-  const hasRun = useRef<{ [key: string]: boolean }>({})
-  useEffect(() => {
-    sendToMixpanel(PAGE_VIEW)
-    return () => {
-      pathname = ''
-    }
-  }, [pathname])
+  let pathname = usePathname();
 
-  return <></>
+  const hasRun = useRef<{ [key: string]: boolean }>({});
+  // if (!hasRun.current[pathname]) {
+
+  useEffect(() => {
+    sendToMixpanel(PAGE_VIEW);
+    return () => {
+      pathname = ""
+    }
+  }, [pathname]);
+    
+
+  return <>
+  </>;
 }
