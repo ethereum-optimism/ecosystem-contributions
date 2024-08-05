f008a0eed4c5cd4548c3d28b17e614ef442077e5
tgrziminiar <mixzazazaza1212@gmail.com>
2024-08-05 12:26:00 +0700

    feat: add mixpanel event name

---
+++ public/static/mixpanel/event-name.ts
@@ -0,0 +1,43 @@
+// Each of the key have a unique properties depends on event
+
+// src/app/component/routeListener.tsx
+const PAGE_VIEW = "Page View"
+
+// /src/app/(navbar)/(home)/component/Tab.tsx
+const BUILDER_IDEA_CLICK = "Builder Ideas Click"
+
+const CLIAMS_IDEA_CLICK = "Cliams Idea Click"
+const JOIN_DISCUSSION_CLICK = "Join Discussion Click"
+const DEVELOPER_TOOLING_CLICK = "Developer Tooling Click"
+const DEVELOPER_ONBOARDING_CLICK = "Developer Onboarding Click"
+const DEVELOPER_EXPERIMENTATION_CLICK = "Developer Experimentation Click"
+const PROTOCOL_INFRASTRUCTURE_CLICK = "Protocol Infrastructure Click"
+const DAPP_IDEA_CLICK = "Dapp Idea Click"
+const LABEL_FILTER_SELECT = "Label Filter Select"
+const EXECUTION_STATUS_FILTER_SELECT = "Execution Status Filter Select"
+const EFFORT_FILTER_SELECT = "Effort Filter Select"
+const SKILL_SET_FILTER_SELECT = "Skill Set Filter Select"
+
+const NAV_RETROPGF_CLICK = "RetroPGF Click"
+const NAV_BRIDGE_CLICK = "Bridge Click"
+const NAV_AIRDROP_CLICK = "AirDrop Click"
+
+
+export {
+    BUILDER_IDEA_CLICK, 
+    PAGE_VIEW,
+    CLIAMS_IDEA_CLICK,
+    JOIN_DISCUSSION_CLICK,
+    DEVELOPER_TOOLING_CLICK,
+    DEVELOPER_ONBOARDING_CLICK,
+    DEVELOPER_EXPERIMENTATION_CLICK,
+    PROTOCOL_INFRASTRUCTURE_CLICK,
+    DAPP_IDEA_CLICK,
+    LABEL_FILTER_SELECT,
+    EXECUTION_STATUS_FILTER_SELECT,
+    EFFORT_FILTER_SELECT,
+    SKILL_SET_FILTER_SELECT,
+    NAV_RETROPGF_CLICK,
+    NAV_BRIDGE_CLICK,
+    NAV_AIRDROP_CLICK,
+}
